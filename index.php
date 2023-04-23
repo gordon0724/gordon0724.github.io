8212dd5af5f8b7c76981e6ba85e2658a73f65216
gordon0724 <54224954+gordon0724@users.noreply.github.com>
2023-04-24 01:27:38 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>網頁程式設計與安全實務</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="static/style.css">
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  </head>
+  <body>
+    <nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand"><span class="glyphicon glyphicon-education"></span> <font face="微軟正黑體">網頁程式設計與安全實務</font></a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="index.php"><font face="微軟正黑體">首頁 </font><span class="glyphicon glyphicon-home" aria-hidden="true"></span></a></li>
+      <li><a href="calendar.php"><font face="微軟正黑體">月曆 </font></a></li>
+	  <li><a href="schedule.php"><font face="微軟正黑體">功課表呈現 </font></a></li>
+    </ul>
+  </div>
+</nav>
+  
+  <div class="container">
+    <div class="col-md-4 col-md-offset-4">
+      <img class="img-responsive img-circle" src="static/profile.jpg">
+    </div>
+    <div class="col-md-6 col-md-offset-3">
+            <h2><center><font face="微軟正黑體">20230403作業</font></center></h2>
+            <table class="table table-striped">
+                <thead>
+                  <tr class="warning">
+                    <th class="text-center"></th>
+                    <th></th>
+                  </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                      <td class="text-center"><span class="glyphicon glyphicon-tag"></span> 作業1</td>
+                      <td class="text-center"><a href="calendar.php"><font face="微軟正黑體">月曆 </font></a></td>
+                    </tr>
+                    <tr>
+                      <td class="text-center"><span class="glyphicon glyphicon-tag"></span> 作業2</td>
+                      <td class="text-center"><a href="schedule.php"><font face="微軟正黑體">功課表呈現 </font></a></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+  </body>
+</html>
